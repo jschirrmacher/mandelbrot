834f0852b3ec2b5c9426bdaafd8f3cda91a905e8
Christian Gräfe <chr.graefe@gmail.com>
2020-03-13 12:23:04 +0100

    Using of Release configuration is much faster
    The usage of the Release configuration is much fast as the default Debug mode

--- c#/run.sh
+++ c#/run.sh
@@ -2,5 +2,5 @@
 
 VERSION=$(dotnet --version)
 cd $(dirname "$0")
-LANG=en_EN.UTF-8 dotnet run -p Mandelbrot.csproj | sed -e "s/C#/C# ${VERSION}/"
+LANG=en_EN.UTF-8 dotnet run -p Mandelbrot.csproj -c Release | sed -e "s/C#/C# ${VERSION}/"
 cd ..
